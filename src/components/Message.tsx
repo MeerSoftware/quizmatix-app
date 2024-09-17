6daae8d11422e9e76013eb2ed67b72fa98476d41
bariscodefx <bariscodefx@protonmail.com>
2024-09-18 00:03:40 +0300

    Refactored Message.tsx

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,27 +1,28 @@
 'use client';
 
-function Message({ text, author, authorId, profilePicture }: { text: string, author: string, authorId: number, profilePicture: string }) {
-    let chatClass: string = 'chat chat-';
-    let baseURLProfiles: string = '//quizmatix.com/client/assets/profiles/';
-    if (authorId === parseInt(localStorage.getItem('id') ?? '')) {
-        chatClass += 'end';
-    } else {
-        chatClass += 'start';
-    }
+interface MessageProps {
+    text: string;
+    author: string;
+    authorId: number;
+    profilePicture: string;
+}
+
+const Message: React.FC<MessageProps> = ({ text, author, authorId, profilePicture }) => {
+    const chatClass = `chat chat-${authorId === parseInt(localStorage.getItem('id') ?? '') ? 'end' : 'start'}`;
+    const baseURLProfiles = '//quizmatix.com/client/assets/profiles/';
+    const profileSrc = profilePicture ? `${baseURLProfiles}${profilePicture}` : "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg";
+
     return (
         <div className={chatClass}>
             <div className="chat-image avatar">
                 <div className="w-10 rounded-full">
-                    <img
-                        src={(baseURLProfiles + profilePicture) ?? "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"} />
+                    <img src={profileSrc} alt={`${author}'s profile`} />
                 </div>
             </div>
-            <div className="chat-header">
-                {author}
-            </div>
+            <div className="chat-header">{author}</div>
             <div className="chat-bubble break-words max-w-[70vw]">{text}</div>
         </div>
     );
-}
+};
 
 export default Message;
